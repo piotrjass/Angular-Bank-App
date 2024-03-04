2440e2b9fb78ba5804a8ecc440e9a5808d255d2a
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-03-05 00:45:54 +0100

    Working on credit cards

--- src/app/core/components/user-dashboard/credits-card-container/credits-card-container.component.ts
+++ src/app/core/components/user-dashboard/credits-card-container/credits-card-container.component.ts
@@ -8,4 +8,19 @@ import { CreditCardComponent } from '../credit-card/credit-card.component';
   templateUrl: './credits-card-container.component.html',
   styleUrl: './credits-card-container.component.css',
 })
-export class CreditsCardContainerComponent {}
+export class CreditsCardContainerComponent {
+  cards: any[] = [
+    {
+      color: 'bg-gradient-to-r from-cyan-200 to-blue-200',
+      balance: 720,
+      last_digits: '4334',
+      exp_date: '03/27',
+    },
+    {
+      color: 'bg-gradient-to-r from-green-200  to-teal-200',
+      balance: 440,
+      last_digits: '0681',
+      exp_date: '12/24',
+    },
+  ];
+}
