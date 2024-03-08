5e605966c6e11f8c2af01daac8d7e9b1d5870879
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-03-08 22:50:44 +0100

    Working on auth

--- src/app/core/components/login/login-form/login-form.component.ts
+++ src/app/core/components/login/login-form/login-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../../../shared/services/auth.service';
 
 @Component({
   selector: 'app-login-form',
@@ -11,8 +12,24 @@ import { RouterLink } from '@angular/router';
   styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
+  constructor(private authService: AuthService) {}
+  login() {
+    this.authService.login();
+  }
   onSubmit(f: NgForm) {
     console.log(f.value);
     console.log(f.valid);
   }
 }
+// export class CreditsCardContainerComponent {
+//   cards: any[] = [];
+//   constructor(private cardsService: CardsService) {}
+
+//   ngOnInit(): void {
+//     this.cards = this.cardsService.getCards();
+//   }
+
+//   test() {
+//     this.cardsService.getCardsFromServer();
+//   }
+// }
